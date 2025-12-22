92bc106d2e59227dd3ff8ccfe630730df77d9823
abdullahprince457 <148543004+abdullahprince457@users.noreply.github.com>
2025-12-22 08:17:20 +0000

    data

---
+++ lec 6 datatypes2/index.js
@@ -0,0 +1,23 @@
+// let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber )
+// console.log(valueInNumber)
+
+// let isLoggedIn = "abdullah"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => 
+
+let someNumber = 33
+
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
