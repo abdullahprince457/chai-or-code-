709a36f8a898ba042966d5e20d244ad4e699f14c
abdullahprince457 <148543004+abdullahprince457@users.noreply.github.com>
2025-12-22 09:29:54 +0000

    post / pre incre

---
+++ lec7/string.js
@@ -0,0 +1,28 @@
+// let value = 3
+// let negValue = -value
+
+
+// console.log(negValue)
+
+let str1 = "Abdullah"
+let str2 = " Shahzad"
+
+let str3 = str1 + str2
+
+console.log(str3)
+
+
+console.log("1" + 2) /12
+console.log(1 + "2") /12
+console.log("1" + 2 + 2) /122
+console.log(1 + 1 +"2") /32 
+
+let x = 3
+let y = x++
+
+console.log(`x:${x} , y:${y}`)
+
+let x = 3
+let y = ++x
+
+console.log(`x:${x} , y:${y}`)
