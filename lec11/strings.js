b95b9b8c8bf584c9f3b40f940344c0222e1987a4
abdullahprince457 <148543004+abdullahprince457@users.noreply.github.com>
2025-12-29 13:55:52 +0000

    lec11

---
+++ lec11/strings.js
@@ -0,0 +1,27 @@
+let name = "Abdullah"
+let age = 22
+
+// console.log(`My name is ${name} and my age is ${ age}`)
+
+// const gameName = new String('Abdullah')
+// console.log(gameName[6])
+// console.log(gameName.toUpperCase())
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('u'))
+// console.log(gameName.substring(0 , 4))
+
+// const newString = gameName.substring(0 , 4)
+// console.log(newString)
+
+// const anotherString = gameName( -8 , 4)
+// console.log(anotherString)
+
+// const newStringOne = "      Abdullah     "
+// console.log(newStringOne)
+
+const url = "https://www.Abdullah/com/abdullah%20shahzad"
+console.log(url.replace('20%','-'))
+
+console.log(url.includes('sundar'))
