512a130dd9471b6cab4a7a2f1fa90585aa3bcf1f
abdullahprince457 <148543004+abdullahprince457@users.noreply.github.com>
2026-01-06 12:11:40 +0000

    scope

---
+++ basics(3)/lec22_scopes.js
@@ -0,0 +1,35 @@
+function one(){
+    const username = "Abdullah"
+
+    function two(){
+        const website = "youtube "
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+one()
+
+if (true){
+    const username = "Abdullah"
+    if (username === "Abdullah"){
+        const website = " Youtube"
+        console.log(username + website)
+    }
+}
+
+// +++++++++++++++++++++intresting+++++++++++++++++++++
+
+addone(5)
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
